e761bfff588477af227bec3bfaade3688fe7fc14
zhsyoo <zhsyoo@naver.com>
2025-12-21 12:23:31 +0900

    fix: docker run after mkdir -p datasets

--- docker_run.sh
+++ docker_run.sh
@@ -7,6 +7,8 @@ TAG="latest"
 docker stop ${MODULE}
 docker rm ${MODULE}
 
+mkdir -p datasets
+
 # docker run --rm -p 6003:7860 \
 docker run --restart unless-stopped -d -p 6003:7860 \
     -e VITE_APP_NODE_ENV="prod" \
