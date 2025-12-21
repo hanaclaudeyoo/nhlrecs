11509293db55a7b82230c5255cd78352eb86fb8a
zhsyoo <zhsyoo@gmail.com>
2025-12-21 12:15:23 +0900

    fix: docker datasets volume

--- docker_run.sh
+++ docker_run.sh
@@ -10,5 +10,6 @@ docker rm ${MODULE}
 # docker run --rm -p 6003:7860 \
 docker run --restart unless-stopped -d -p 6003:7860 \
     -e VITE_APP_NODE_ENV="prod" \
+    -v $PWD/datasets:/home/app/datasets \
     --name ${MODULE} \
     ${PACKAGE}/${MODULE}:${TAG} 
